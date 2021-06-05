3364e7a31470aaabf838fef52b11977bfc50a16d
mayavathi <mayachandre27@gmail.com>
2021-06-05 00:22:26 -0400

    Flipping a coin heads or tails

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 "echo " Welcome to flip coin combination program"
+declare -a countS
+declare -a countD
+declare -a countT
+head=1
+tail=0
+function characterConvert( ){
+
+      if [ $1 -eq 1 ]
+      then
+           echo "H"
+       else  
+           echo "T"
+fi
+
+}
 
