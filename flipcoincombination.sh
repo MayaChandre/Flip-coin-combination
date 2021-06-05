4dd8da46cb1eee5852b5bfcd4d4394d48f8f82ae
mayavathi <mayachandre27@gmail.com>
2021-06-05 00:37:01 -0400

    Singlet combination in dictionary

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -17,3 +17,24 @@ fi
 
 }
 
+# Simulate single combination
+for ((i=0;i<50; i++))
+do
+       random=$((RANDOM%2))
+       randomC1=$(characterconvert $random1)
+       if [ -v countS[$randomC1]}
+       then
+            ((countS[randomC1]++))
+      else
+            countS[$randomC1]=1
+fi
+done
+echo "Winner in single combination"
+for key in ${!countS[@]}"
+do
+      # echo " percentage of $key =" $(($countS[$key]} * 100 /50))
+      # echo $(($countS[$key]} 
+       echo $key ' - ' $(($countS[$key]} 
+done | sort -rn -k3 | head -1
+#echo $countS[$@]}
+
